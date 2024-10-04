ddabafc8b9f2013b103a4a0248396cf8897f7ce9
Asari <2364592@cognizant.com>
2024-10-04 12:35:41 +0900

    new test

--- tests/demo.spec.js
+++ tests/demo.spec.js
@@ -1,6 +1,6 @@
 import { test, expect } from '@playwright/test';
 
-test('Login - Valid User', async ({page}) =>
+test('1 - Login - Valid User', async ({page}) =>
     {
     
         //go to url
@@ -15,7 +15,7 @@ test('Login - Valid User', async ({page}) =>
     
     })
 
-test('Login - Locked out user', async ({page}) =>
+test('2 - Login - Locked out user', async ({page}) =>
     {
     
         //go to url
@@ -33,8 +33,7 @@ test('Login - Locked out user', async ({page}) =>
     
     })
 
-
-test('Complete Order Flow - Single Product', async ({page}) =>
+test('3 - Complete Order Flow - Single Product', async ({page}) =>
 {
 
     //go to url
@@ -96,7 +95,7 @@ test('Complete Order Flow - Single Product', async ({page}) =>
 
 })
 
-test('Cart - Multiple Products(First 3 products)', async ({page}) =>
+test('4 - Cart - Multiple Products(First 3 products)', async ({page}) =>
     {
     
         //go to url
@@ -143,56 +142,136 @@ test('Cart - Multiple Products(First 3 products)', async ({page}) =>
     
     })
     
-    test.only('Cart - Multiple Products(Fixed Products)', async ({page}) =>
-        {
+test('5 - Cart - Multiple Products(Fixed Products)', async ({page}) =>
+    {
+    
+        //go to url
+        await page.goto("https://www.saucedemo.com/v1/");
+        //login
+        await page.locator("[data-test='username']").fill("standard_user");
+        await page.locator("[data-test='password']").fill("secret_sauce");
+        await page.locator("[id='login-button']").click();
+        //logged-in
+        await page.waitForLoadState('networkidle');
+        await expect(page.locator("[class='product_label']")).toContainText("Products");
+        //selecting product
+    
+        const products = await page.locator("[class='inventory_item']")
+        const count = await products.count();
+        const productCount = 3
+        //product name
+        const productName = ["Sauce Labs Backpack",
+            "Sauce Labs Bike Light",
+            "Sauce Labs Onesie"
+        ];
+        for(let i=0; i<count; i++)
+            {   
+                const allProducts = await products.nth(i).locator("div >> a >> div").textContent();
+                // //printing trimmed products
+                // console.log(await allProducts.trim());
+                //logic to check if product is from the list
+                if(productName.includes(allProducts.trim())){
+                    await products.nth(i).locator("button").click();
+                }
+            }        
+        //verify product added to cart
+        const badgeIcon = await page.locator("[class*='cart_badge']");
+        await expect(badgeIcon).toContainText(String(productCount));
+        await badgeIcon.click();
+        // await page.pause();
         
-            //go to url
-            await page.goto("https://www.saucedemo.com/v1/");
-            //login
-            await page.locator("[data-test='username']").fill("standard_user");
-            await page.locator("[data-test='password']").fill("secret_sauce");
-            await page.locator("[id='login-button']").click();
-            //logged-in
-            await page.waitForLoadState('networkidle');
-            await expect(page.locator("[class='product_label']")).toContainText("Products");
-            //selecting product
+        //verify all products names in cart
         
-            const products = await page.locator("[class='inventory_item']")
-            const count = await products.count();
-            const productCount = 3
-            //product name
-            const productName = ["Sauce Labs Backpack",
-                "Sauce Labs Bike Light",
-                "Sauce Labs Onesie"
-            ];
-            for(let i=0; i<count; i++)
-                {   
-                    const allProducts = await products.nth(i).locator("div >> a >> div").textContent();
-                    //printing trimmed products
-                    console.log(await allProducts.trim());
-                    if(productName.includes(allProducts.trim())){
-                        await products.nth(i).locator("button").click();
-                    }
-                }        
-            //verify product added to cart
-            const badgeIcon = await page.locator("[class*='cart_badge']");
-            await expect(badgeIcon).toContainText(String(productCount));
-            await badgeIcon.click();
-            // await page.pause();
-            
-            //verify all products names in cart
-            
-            for(let i=0; i<productCount; i++){
-                const productCheckedOut = await page.locator("[class='inventory_item_name']").nth(i);
-                console.log(await productCheckedOut.textContent()); //priting checkout item
-                await expect(productCheckedOut).toContainText(productName[i]);
-            }
-            
-           
-           //pause
-            // await page.pause();
+        for(let i=0; i<productCount; i++){
+            const productCheckedOut = await page.locator("[class='inventory_item_name']").nth(i);
+            console.log(await productCheckedOut.textContent()); //priting checkout item
+            await expect(productCheckedOut).toContainText(productName[i]);
+        }
         
         
-        
-        })
+        //pause
+        // await page.pause();
+    
+    
+    
+    })
 
+test.only('6 - Cart - Total Sum of the Order in Cart)', async ({page}) =>
+    {
+    
+        //go to url
+        await page.goto("https://www.saucedemo.com/v1/");
+        //login
+        await page.locator("[data-test='username']").fill("standard_user");
+        await page.locator("[data-test='password']").fill("secret_sauce");
+        await page.locator("[id='login-button']").click();
+        //logged-in
+        await page.waitForLoadState('networkidle');
+        await expect(page.locator("[class='product_label']")).toContainText("Products");
+        //selecting product
+    
+        const products = await page.locator("[class='inventory_item']");
+        const price = await page.locator("[class='inventory_item_price']");
+        const count = await products.count();
+        const productCount = 3;
+        //product name
+        const productName = ["Sauce Labs Backpack",
+            "Sauce Labs Bike Light",
+            "Sauce Labs Onesie"
+        ];
+
+        let cartTotals = 0;
+        for(let i=0; i<count; i++)
+            {   
+                const allProducts = await products.nth(i).locator("div >> a >> div").textContent();
+                // //printing trimmed products
+                // console.log(await allProducts.trim());
+                //logic to check if product is from the list
+                if(productName.includes(allProducts.trim())){
+                    const total = await price.nth(i).textContent();
+                    const totalArray = total.split(' ');
+                    console.log(await totalArray); 
+                    for (let i=0; i<totalArray.length; i++ ){
+                    const totals = Number(totalArray[i].replace('$', ''));
+                    cartTotals += totals
+                    }
+                //proceed to add products       
+                    await products.nth(i).locator("button").click();
+                }
+            }     
+        console.log(await cartTotals); //priting Cart amount
+   
+        //verify product added to cart
+        const badgeIcon = await page.locator("[class*='cart_badge']");
+        await expect(badgeIcon).toContainText(String(productCount));
+        await badgeIcon.click();
+        // await page.pause();
+        
+        //verify all products names in cart
+        
+        for(let i=0; i<productCount; i++){
+            const productCheckedOut = await page.locator("[class='inventory_item_name']").nth(i);
+            await expect(productCheckedOut).toContainText(productName[i]);
+        }
+        
+        await page.locator("[class*='checkout_button']").click();
+        //fill up checkout information
+        await page.locator("[id='first-name']").fill("Foo");
+        await page.locator("[id='last-name']").fill("bar");
+        await page.locator("[id='postal-code']").fill("00000");
+        await page.locator("[type='submit']").click();
+
+        //verify amount in order sumarry is same as cart amount
+        const sumAmount = await page.locator("[class='summary_subtotal_label']").textContent();
+        const finalAmount = await Number(sumAmount.split(' ').pop().replace('$', ''));
+        console.log(await finalAmount)
+        await expect(cartTotals).toBe(finalAmount);
+
+        //pause
+        // await page.pause();
+    
+
+        
+        
+    
+    })
