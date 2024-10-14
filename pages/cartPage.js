1478620e624f9c565aa4b61e07cb09bf04e89155
Asari <2364592@cognizant.com>
2024-10-14 15:46:53 +0900

    pom implementation

--- pages/cartPage.js
+++ pages/cartPage.js
@@ -6,26 +6,99 @@ class CartPage {
     this.page = page;
     this.cartItems = page.locator("[class='inventory_item_name']");
     this.checkout = page.locator("[class*='checkout_button']");
+    this.amount = page.locator("[class='inventory_item_price']");
+    
 }
 
-async verifyCart(productName)
-{
 
-    const cartVerified = expect(this.cartItems).toContainText(productName);
-    expect (cartVerified).toBeTruthy();
-     
+async getCartList(countType)
+{
+    {   
+        let count;
+        if(countType === "allProducts")
+        {
+            count = await this.cartItems.count();
+        }
+        else if(countType === "fixedQuantity")
+        {
+            count = 3; 
+        }
+        
+        const productText = [];
+        for(let i=0; i<count; i++)
+            {
+                const text = await this.cartItems.nth(i).textContent();
+                if (text){
+                    productText.push(text.trim());
+
+                }
+                else{
+                    console.warn("undefined");
+                }
+
+            }
+        return productText;
+    }
+    
 }
 
-async getCartList()
+
+async verifyCart(productName, countType)
 {
-    return await this.cartItems.textContent();
+
+    let count;
+    if(countType === "allProducts")
+    {
+        count = await this.cartItems.count();
+    }
+    else if(countType === "fixedQuantity")
+    {
+        count = 3; 
+    }
+
+    for(let i=0; i<count; i++)
+        {
+            const productCheckedOut = await this.cartItems.nth(i);
+            const cartVerified = expect(productCheckedOut).toContainText(productName[i]);
+            expect (cartVerified).toBeTruthy();
+        }           
+}
+
+
+async verifyCartAmount(countType)
+{
+    let count;
+    if(countType === "allProducts")
+    {
+        count = await this.cartItems.count();
+    }
+    else if(countType === "fixedQuantity")
+    {
+        count = 3; 
+    }
+
+    const amountText = [];
+    let totalCartAmount = 0;
+    for(let i=0; i<count; i++){
+        const itemCartAmount = await this.amount.nth(i).textContent();
+        amountText.push(itemCartAmount.trim());
+    }
+    for(let i=0; i<amountText.length; i++){
+        const itemCartAmounts = Number(amountText[i]);
+        totalCartAmount += itemCartAmounts;
+        }
+return totalCartAmount;
+}
+
+async compareTotalCart(getTotal, getCartTotal)
+{
+    await expect(getTotal).toBe(getCartTotal);
 }
 
 async checkingOut()
 {
-await this.checkout.click();
+    await this.checkout.click();
 }
-
 }
 
 module.exports = {CartPage};
--- pages/orderreviewPage.js
+++ pages/orderreviewPage.js
@@ -7,19 +7,48 @@ class OrderReviewPage {
     this.orderCompletedProducts = page.locator("[class='inventory_item_name']");
     this.refNumber = page.locator("[class='summary_value_label']").nth(0);
     this.completedOrder = page.locator("[class='btn_action cart_button']");
+    this.sumAmount = page.locator("[class='summary_subtotal_label']");
 
 }
 
-async verifyOrderComplete(productName)
+async verifyOrderComplete(productName, countType)
 {
-    await expect(this.orderCompletedProducts).toContainText(productName);
 
+    let count;
+    if(countType === "allProducts")
+    {
+        count = await this.orderCompletedProducts.count();
+    }
+    else if(countType === "fixedQuantity")
+    {
+        count = 3; 
+    }
+
+    for(let i=0; i<count; i++)
+        {
+            const productCompleted = await this.orderCompletedProducts.nth(i);
+            const orderReviewed = expect(productCompleted).toContainText(productName[i]);
+            expect (orderReviewed).toBeTruthy();
+        }           
 }
 
 async verifyReferenceNumber()
 {
     const getrefNum = await this.refNumber.textContent();
     return await getrefNum;
+}
+
+async orderTotal()
+{
+    //verify amount in order sumarry is same as cart amount
+    const sumAmounts = await this.sumAmount.textContent();
+    const finalAmount = Number(sumAmounts.split(' ').pop().replace('$', ''));
+    return finalAmount
+}
+
+async compareTotalPDP(getTotal, getORTotal)
+{
+    await expect(getTotal).toBe(getORTotal);
 }
 
 async completeOrder()
