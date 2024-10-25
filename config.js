840fdb546ccf8ccd78744e447e48e06dc7ffa257
Asari <2364592@cognizant.com>
2024-10-25 18:54:03 +0900

    test parameterization

---
+++ config.js
@@ -0,0 +1,8 @@
+//change environment to staging or production when needed
+
+
+
+const environment = process.env.TEST_ENV || "production";
+//run is >> TEST_ENV=production npx cucumber-js or change the default value above
+
+module.exports = { environment };
